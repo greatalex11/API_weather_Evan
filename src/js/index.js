16e140059824e319c43f009cd7935d3adef2a4b9
EvanMlj <120018761+EvanMlj@users.noreply.github.com>
2023-06-30 09:24:18 +0200

    feat/dev/t2/AddMapSettingsGeolocPopUpErrorMessage

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,40 @@
+// Initialisation de la map OpenstreetMap
 let map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+
+// Indique longitude et Latitude quanďla map est cliqué
+let popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
+
+// Geolocalisation 
+map.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+map.on('locationfound', onLocationFound);
+
+//Message d'erreur Geolocalisation 
+function onLocationError(e) {
+    alert(e.message);
+}
+map.on('locationerror', onLocationError);
