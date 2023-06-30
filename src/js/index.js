c7e11bcc7d8d52222abe6a534eb0b4a5e371a27b
EvanMlj <120018761+EvanMlj@users.noreply.github.com>
2023-06-30 15:42:35 +0200

    feat/dev/t2/AddSettingToggleForOneHTML

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,18 @@
+let settingIcon = document.querySelector('#setting_icon') ; 
+let formulaireSetting = document.querySelector('#formulaireSettings')
+let mainPage = document.querySelector('#mainPage') ; 
+let settingPage =  document.querySelector('.ctn_setting_page') ;
+console.log('settingIcon') ;
+console.log('formulaireSetting') ; 
+console.log('mainPage') ; 
+console.log('settingPage') ; 
+
+
+settingIcon.addEventListener('click', function (){
+    mainPage.style.display = "none" ;
+    settingPage.style.display = "flex" ; 
+})
+
 // Initialisation de la map OpenstreetMap
 let map = L.map('map').setView([51.505, -0.09], 13);
 
@@ -5,8 +20,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
-
 
 // Indique longitude et Latitude quanďla map est cliqué
 let popup = L.popup();
@@ -27,7 +40,7 @@ function onLocationFound(e) {
     var radius = e.accuracy;
 
     L.marker(e.latlng).addTo(map)
-        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+        .bindPopup("Vous êtes ici " + radius).openPopup();
 
     L.circle(e.latlng, radius).addTo(map);
 }
@@ -38,3 +51,12 @@ function onLocationError(e) {
     alert(e.message);
 }
 map.on('locationerror', onLocationError);
+
+
+// //desactive submit 
+ formulaireSetting.addEventListener("submit", function(event) {
+     // Empêche le comportement par défaut du bouton "submit" car sinon la page se rafraichit
+    event.preventDefault(); 
+});
+
+
