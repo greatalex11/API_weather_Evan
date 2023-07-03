b6bd4f43bdc4718cc3b181cf7eb8944678eef0ea
EvanMlj <120018761+EvanMlj@users.noreply.github.com>
2023-07-03 10:37:23 +0200

    fix/dev/t2/Map2SettingPageHtmlCss

--- src/js/index.js
+++ src/js/index.js
@@ -25,21 +25,17 @@ weatherIcon.addEventListener('click', function () {
 
 // Initialisation de la map OpenstreetMap
 let map = L.map('map').setView([51.505, -0.09], 13);
-let map2 = L.map('map2').setView([51.505, -0.09], 13);
+
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// add the same OpenStreetMap tile layer to the second map
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map2);
 
 // Indique longitude et Latitude quanďla map est cliqué
 let popup = L.popup();
+
 
 function onMapClick(e) {
     popup
@@ -49,6 +45,7 @@ function onMapClick(e) {
 }
 
 map.on('click', onMapClick);
+
 
 // Geolocalisation 
 map.locate({setView: true, maxZoom: 16});
@@ -69,6 +66,44 @@ function onLocationError(e) {
 }
 map.on('locationerror', onLocationError);
 
+
+//Initialisation et geocalisation pour map2
+
+let map2 = L.map('map2').setView([51.505, -0.09], 13);
+    // add the same OpenStreetMap tile layer to the second map
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map2);
+
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map2);
+}
+
+map2.on('click', onMapClick);
+
+    // Geolocalisation 
+map2.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(map2)
+        .bindPopup("Vous êtes ici " + radius).openPopup();
+
+    L.circle(e.latlng, radius).addTo(map2);
+}
+map2.on('locationfound', onLocationFound);
+
+    //Message d'erreur Geolocalisation 
+function onLocationError(e) {
+    alert(e.message);
+}
+map2.on('locationerror', onLocationError);
 
 
 // //desactive submit 
