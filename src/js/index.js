6ffbc08c0a5f165ae131dce4a41c1a8578a3048d
EvanMlj <120018761+EvanMlj@users.noreply.github.com>
2023-07-04 16:45:23 +0200

    feat/dev/t2/RslyRechToLongLatMapGoToRsltRech

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+
 let settingIcon = document.querySelector('#setting_icon') ; console.log(settingIcon) ;
 let formulaireSetting = document.querySelector('#formulaire') ; console.log(formulaireSetting) ;
 let mainPage = document.querySelector('#mainPage') ; console.log(mainPage) ;
@@ -11,7 +12,6 @@ let weatherIcon = document.querySelector('.weatherIcon') ; console.log(weatherIc
 settingIcon.addEventListener('click', function() {
     mainPage.classList.toggle('hidden');
     settingPage.classList.toggle('visible');
-
   });
 
 weatherIcon.addEventListener('click', function () {
@@ -21,8 +21,10 @@ weatherIcon.addEventListener('click', function () {
 
 
 // Initialisation de la map OpenstreetMap
-let map = L.map('map').setView([51.505, -0.09], 13);
 
+
+let map = L.map('map').setView([41.33,19.82], 15);
+     // Inverser les deux chiffre par rapport à capitals_europe_master.json
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -37,83 +39,97 @@ let popup = L.popup();
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
+        .setContent("Vous êtes ici ! " + e.latlng.toString())
         .openOn(map);
 }
 
 map.on('click', onMapClick);
 
 
+
 // Geolocalisation 
-map.locate({setView: true, maxZoom: 16});
+// map.locate({setView: true, maxZoom: 16});
 
-function onLocationFound(e) {
-    var radius = e.accuracy;
+// function onLocationFound(e) {
+//     var radius = e.accuracy;
 
-    L.marker(e.latlng).addTo(map)
-        .bindPopup("Vous êtes ici " + radius).openPopup();
+//     L.marker(e.latlng).addTo(map)
+//         .bindPopup("Vous êtes ici " + radius).openPopup();
 
-    L.circle(e.latlng, radius).addTo(map);
-}
-map.on('locationfound', onLocationFound);
+//     L.circle(e.latlng, radius).addTo(map);
+// }
+// map.on('locationfound', onLocationFound);
 
-//Message d'erreur Geolocalisation 
-function onLocationError(e) {
-    alert(e.message);
-}
-map.on('locationerror', onLocationError);
+// //Message d'erreur Geolocalisation 
+// function onLocationError(e) {
+//     alert(e.message);
+// }
+// map.on('locationerror', onLocationError);
 
 
 //Initialisation et geocalisation pour map2
 
-let map2 = L.map('map2').setView([51.505, -0.09], 13);
-    // add the same OpenStreetMap tile layer to the second map
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map2);
+// let map2 = L.map('map2').setView([51.505, -0.09], 13);
+//     // add the same OpenStreetMap tile layer to the second map
+// L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map2);
 
 
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map2);
-}
+// function onMapClick(e) {
+//     popup
+//         .setLatLng(e.latlng)
+//         .setContent("You clicked the map at " + e.latlng.toString())
+//         .openOn(map2);
+// }
 
-map2.on('click', onMapClick);
+// map2.on('click', onMapClick);
 
-    // Geolocalisation 
-map2.locate({setView: true, maxZoom: 16});
+//     // Geolocalisation 
+// map2.locate({setView: true, maxZoom: 16});
 
-function onLocationFound(e) {
-    var radius = e.accuracy;
+// function onLocationFound(e) {
+//     var radius = e.accuracy;
 
-    L.marker(e.latlng).addTo(map2)
-        .bindPopup("Vous êtes ici " + radius).openPopup();
+//     L.marker(e.latlng).addTo(map2)
+//         .bindPopup("Vous êtes ici " + radius).openPopup();
 
-    L.circle(e.latlng, radius).addTo(map2);
-}
-map2.on('locationfound', onLocationFound);
+//     L.circle(e.latlng, radius).addTo(map2);
+// }
+// map2.on('locationfound', onLocationFound);
 
-    //Message d'erreur Geolocalisation 
-function onLocationError(e) {
-    alert(e.message);
-}
-map2.on('locationerror', onLocationError);
-
-
+//     //Message d'erreur Geolocalisation 
+// function onLocationError(e) {
+//     alert(e.message);
+// }
+// map2.on('locationerror', onLocationError);
 
 
 //desactive submit 
- formulaireSetting.addEventListener("submit", function(event) {
+ document.getElementById("validation").addEventListener("click", function(event) {
      // Empêche le comportement par défaut du bouton "submit" car sinon la page se rafraichit
     event.preventDefault(); 
     //recupere le rslt de la recherche
    rsltRech = document.getElementById("recherche").value ;
    //affiche le résultat dans la console
    console.log(rsltRech);
-   //retour page d'accueil
+   
+   fetch('http://api.openweathermap.org/geo/1.0/direct?q='+rsltRech+'&limit=1&appid=308072db4828ee7f23b0fe63c3dd9918')
+   .then(response => response.json())
+   .then(data => {
+        console.log(data[0].lat);
+        console.log(data[0].lon);
+        map.setView([data[0].lat,data[0].lon], 15);
+        fetch('https://api.openweathermap.org/data/2.5/weather?lat='+data[0].lat+'&lon='+data[0].lon+'&appid=308072db4828ee7f23b0fe63c3dd9918')
+        .then(response => response.json())
+        .then(data => { 
+             console.log(data);
+         });
+    });
+
+    
+    //retour page d'accueil
    mainPage.classList.toggle('visible');
    settingPage.classList.toggle('hidden');
 
