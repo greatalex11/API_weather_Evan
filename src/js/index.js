f427bfb0d50d13b596b43e51e57b27fd3f009670
EvanMlj <120018761+EvanMlj@users.noreply.github.com>
2023-07-10 12:10:10 +0200

    feat/dev/t2:Ajout display icone in App

--- src/js/index.js
+++ src/js/index.js
@@ -1,104 +1,142 @@
+let settingIcon = document.querySelector("#setting_icon");
+console.log(settingIcon);
+let formulaireSetting = document.querySelector("#formulaire");
+console.log(formulaireSetting);
+let mainPage = document.querySelector("#mainPage");
+console.log(mainPage);
+let settingPage = document.querySelector(".ctn_setting_page");
+console.log(settingPage);
+let moreInfosPage = document.querySelector(".moreInfoPage");
+console.log(moreInfosPage);
+let weatherIcon = document.querySelector(".weatherIcon");
+console.log(weatherIcon);
 
-let settingIcon = document.querySelector('#setting_icon') ; console.log(settingIcon) ;
-let formulaireSetting = document.querySelector('#formulaire') ; console.log(formulaireSetting) ;
-let mainPage = document.querySelector('#mainPage') ; console.log(mainPage) ;
-let settingPage =  document.querySelector('.ctn_setting_page') ; console.log(settingPage) ; 
-let moreInfosPage = document.querySelector('.moreInfoPage') ; console.log(moreInfosPage)
-let weatherIcon = document.querySelector('.weatherIcon') ; console.log(weatherIcon) ;
-
- 
 // En appuyant sur l'incone d'engrenage le main disparait en lui appliquant un display none et même temps la partie setting page qui est de base en display non apparait avec un display flex cela permet de mimer un changement de page en gardant un seul fichier html
 
-settingIcon.addEventListener('click', function() {
-    mainPage.classList.toggle('hidden');
-    settingPage.classList.toggle('visible');
-  });
+settingIcon.addEventListener("click", function () {
+	mainPage.classList.toggle("hidden");
+	settingPage.classList.toggle("visible");
+});
 
-weatherIcon.addEventListener('click', function () {
-    weatherIcon.classList.toggle('visible');
-    mainPage.classList.toggle('hidden');
-})
-
+weatherIcon.addEventListener("click", function () {
+	weatherIcon.classList.toggle("visible");
+	mainPage.classList.toggle("hidden");
+});
 
 // Initialisation de la map OpenstreetMap
 
+let map = L.map("map").setView([47.23, 6.024], 15);
+// Inverser les deux chiffre par rapport à capitals_europe_master.json
 
-let map = L.map('map').setView([41.33,19.82], 15);
-     // Inverser les deux chiffre par rapport à capitals_europe_master.json
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+	maxZoom: 19,
+	attribution:
+		'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
-
 
 // Indique longitude et Latitude quanďla map est cliqué
 let popup = L.popup();
 
-
 function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("Voici la zone ! " + e.latlng.toString())
-        .openOn(map);
+	popup
+		.setLatLng(e.latlng)
+		.setContent("Voici la zone ! " + e.latlng.toString())
+		.openOn(map);
 }
 
-map.on('click', onMapClick);
+map.on("click", onMapClick);
 
+//desactive submit
+document
+	.getElementById("validation")
+	.addEventListener("click", function (event) {
+		// Empêche le comportement par défaut du bouton "submit" car sinon la page se rafraichit
+		event.preventDefault();
+		//recupere le rslt de la recherche
+		rsltRech = document.getElementById("recherche").value;
+		//affiche le résultat dans la console
+		console.log(rsltRech);
 
-//desactive submit 
-document.getElementById("validation").addEventListener("click", function(event) {
-    // Empêche le comportement par défaut du bouton "submit" car sinon la page se rafraichit
-   event.preventDefault(); 
-   //recupere le rslt de la recherche
-  rsltRech = document.getElementById("recherche").value ;
-  //affiche le résultat dans la console
-  console.log(rsltRech);
-  
-  fetch('http://api.openweathermap.org/geo/1.0/direct?q='+rsltRech+'&limit=1&appid=308072db4828ee7f23b0fe63c3dd9918')
-  .then(response => response.json())
-  .then(data => {
-       console.log(data[0].lat);
-       console.log(data[0].lon);
-       map.setView([data[0].lat,data[0].lon], 15);
-       fetch('https://api.openweathermap.org/data/2.5/weather?lat='+data[0].lat+'&lon='+data[0].lon+'&units=metric&appid=308072db4828ee7f23b0fe63c3dd9918')
-       .then(response => response.json())
-       .then(data => { 
-            console.log(data);
-            document.querySelector("#temp").innerHTML = data.main.temp +"°C" ; 
-        });
-        
-   });
-  
-   //retour page d'accueil
-  mainPage.classList.toggle('visible');
-  settingPage.classList.toggle('hidden');
+		fetch(
+			"http://api.openweathermap.org/geo/1.0/direct?q=" +
+				rsltRech +
+				"&limit=1&appid=308072db4828ee7f23b0fe63c3dd9918"
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data[0].lat);
+				console.log(data[0].lon);
+				map.setView([data[0].lat, data[0].lon], 15);
+				fetch(
+					"https://api.openweathermap.org/data/2.5/weather?lat=" +
+						data[0].lat +
+						"&lon=" +
+						data[0].lon +
+						"&units=metric&appid=308072db4828ee7f23b0fe63c3dd9918"
+				)
+					.then((response) => response.json())
+					.then((data) => {
+						console.log(data);
+                        // permet d'afficher la température sur un nombre entier
+						document.querySelector("#temp").innerHTML =
+							Math.round(data.main.temp) + "°C";
 
-});
+                        // Affiche les icones selon le temps
+						if (data.weather[0].main == "Clear") {
+							weatherIcon.src = "/src/assets/meteo/sun.svg";
+						} else if (data.weather[0].main == "Clouds") {
+							weatherIcon.src = "/src/assets/meteo/cloudy.svg";
+						} else if (data.weather[0].main == "Rain") {
+							weatherIcon.src = "/src/assets/meteo/rain-1.svg";
+						} else if (data.weather[0].main == "Rain") {
+							weatherIcon.src = "/src/assets/meteo/rain-1.svg";
+						}
+                        else if (data.weather[0].main == "Thunderstorm") {
+							weatherIcon.src = "/src/assets/meteo/storm.svg";
+						}
+                        else if (data.weather[0].main == "Snow") {
+							weatherIcon.src = "/src/assets/meteo/snow.svg";
+						}
+					});
+			});
 
+		//retour page d'accueil
+		mainPage.classList.toggle("visible");
+		settingPage.classList.toggle("none");
+	});
 
-//test api OpenWeather 
+// // test changement d'icone
 
-
-// Geolocalisation 
-// map.locate({setView: true, maxZoom: 16});
-
-// function onLocationFound(e) {
-//     var radius = e.accuracy;
-
-//     L.marker(e.latlng).addTo(map)
-//         .bindPopup("Vous êtes ici " + radius).openPopup();
-
-//     L.circle(e.latlng, radius).addTo(map);
+// if(data.weather[0].main == "Clear"){
+//     weatherIcon.src = "../assets/meteo/sun.svg" ;
+// } else if (data.weather[0].main == "Cloud"){
+//     weatherIcon.src = "../assets/meteo/cloudy.svg" ;
+// } else if (data.weather[0].main == ""){
+//     weatherIcon.src = "../assets/meteo/cloudy.svg" ;
 // }
-// map.on('locationfound', onLocationFound);
 
-// //Message d'erreur Geolocalisation 
-// function onLocationError(e) {
-//     alert(e.message);
-// }
-// map.on('locationerror', onLocationError);
+//test api OpenWeather
 
+// Geolocalisation;
+map.locate({ setView: true, maxZoom: 16 });
+
+function onLocationFound(e) {
+	var radius = e.accuracy;
+
+	L.marker(e.latlng)
+		.addTo(map)
+		.bindPopup("Vous êtes ici " + radius)
+		.openPopup();
+
+	L.circle(e.latlng, radius).addTo(map);
+}
+map.on("locationfound", onLocationFound);
+
+//Message d'erreur Geolocalisation
+function onLocationError(e) {
+	alert(e.message);
+}
+map.on("locationerror", onLocationError);
 
 //Initialisation et geocalisation pour map2
 
@@ -109,7 +147,6 @@ document.getElementById("validation").addEventListener("click", function(event) 
 //     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 // }).addTo(map2);
 
-
 // function onMapClick(e) {
 //     popup
 //         .setLatLng(e.latlng)
@@ -119,7 +156,7 @@ document.getElementById("validation").addEventListener("click", function(event) 
 
 // map2.on('click', onMapClick);
 
-//     // Geolocalisation 
+//     // Geolocalisation
 // map2.locate({setView: true, maxZoom: 16});
 
 // function onLocationFound(e) {
@@ -132,11 +169,8 @@ document.getElementById("validation").addEventListener("click", function(event) 
 // }
 // map2.on('locationfound', onLocationFound);
 
-//     //Message d'erreur Geolocalisation 
+//     //Message d'erreur Geolocalisation
 // function onLocationError(e) {
 //     alert(e.message);
 // }
 // map2.on('locationerror', onLocationError);
-
-
-
